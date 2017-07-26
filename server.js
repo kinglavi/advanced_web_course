fd4b7105f93e1ee2fc5500ac712b222564fb49ae
klavior <klavior@gmail.com>
2017-07-26 19:31:42 +0300

    created server.js

--- server.js
+++ server.js
@@ -7,3 +7,27 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var advertisement = require('./routes/advertisement');
+
+var port = 3000;
+
+var app = express();
+
+// View Engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+// Set Statis Folder
+app.use(express.static(path.join(__dirname,'client')));
+
+// Body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/',index);
+app.use('/api',advertisement);
+
+app.listen(port, function(){
+    console.log("Server started on port " + port)
+});
