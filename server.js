465cbbbc7c7d0a2a08ff6d4be62398d0534a6904
kinglavi <klavior@gmail.com>
2017-08-22 21:36:26 +0300

    added new starter

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
-// Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+// ```
+// server.js
+// (c) 2016 David Newman
+// david.r.niciforovic@gmail.com
+// server.js may be freely distributed under the MIT license
+// ```
 
-// Get our API routes
-const api = require('./server/routes/api');
+// *server.js*
 
-const app = express();
+// Require babel hook included to load all subsequent files required by node
+// with the extensions .es6, .es, .jsx, .js and transpile them with babel.
+// This will also automatically require the polyfill.
+require("babel-register");
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+// Load server configuration
+var app = require('./server.conf.js');
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,24 @@
-/* tslint:disable:no-unused-variable */
+import {
+  it,
+  inject,
+  injectAsync,
+  beforeEachProviders,
+  TestComponentBuilder
+} from '@angular/core/testing';
 
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+// Load the implementations that should be tested
+import {App} from './app.component';
+import {AppState} from './app.service';
 
-describe('App: MeanApp', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-  });
+describe('App', () => {
+  // provide our implementations or mocks to the dependency injector
+  beforeEachProviders(() => [
+    AppState,
+    App
+  ]);
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  it('should have a url', inject([ App ], (app) => {
+    expect(app.url).toEqual('https://twitter.com/datatype_void');
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
 });
