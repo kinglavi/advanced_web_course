ee5c888299edfdacf3f60bc8ba831e48ca15f959
kinglavi <klavior@gmail.com>
2017-08-22 21:32:09 +0300

    bla

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# advanced_web_course
-Advance web course final project
+# MeanApp
+
+App to demostrate building an angular 2 app in the MEAN stack with the Angular CLI.
+
+## Set up.
+Make sure you have angular cli installed.
+```bash
+$ npm install -g angular-cli
+```
+
+Clone the repo
+```bash
+$ git clone https://github.com/gangachris/ng2-mean.git
+$ cd ng2-mean
+```
+
+Install dependencies
+```bash
+$ npm install
+```
+
+Run the app
+```bash
+$ npm run build
+```
--- server.js
+++ server.js
@@ -1,33 +1,41 @@
-/**
- * Created by klavior on 26/07/2017.
- */
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+// Get our API routes
+const api = require('./server/routes/api');
 
-var index = require('./routes/index');
-var advertisement = require('./routes/advertisement');
+const app = express();
 
-var port = 3000;
-
-var app = express();
-
-// View Engine
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
-
-// Set Statics Folder
-app.use(express.static(path.join(__dirname,'client')));
-
-// Body Parser MW
+// Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/',index);
-app.use('/api',advertisement);
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(port, function(){
-    console.log("Server started on port " + port)
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
