2ee20df2cee2049173d6e3221b15829dc670784b
Asaf Regev <asaf05@gmail.com>
2017-08-30 18:09:56 +0100

    Add Statistics

--- client/app/ads/ads.component.ts
+++ client/app/ads/ads.component.ts
@@ -2,107 +2,90 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
-import { AdService } from '../services/ad.service';
+import { CatService } from '../services/cat.service';
 import { ToastComponent } from '../shared/toast/toast.component';
-import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 
 @Component({
-  selector: 'app-ads',
+  selector: 'app-cats',
   templateUrl: './ads.component.html',
   styleUrls: ['./ads.component.scss']
 })
 export class AdsComponent implements OnInit {
 
-  ad = {};
-  ads = [];
+  cat = {};
+  cats = [];
   isLoading = true;
   isEditing = false;
-  optionsModel: number[];
-  myOptions: IMultiSelectOption[];
-  screenIds = [];
 
-  addAdForm: FormGroup;
+  addCatForm: FormGroup;
   name = new FormControl('', Validators.required);
   age = new FormControl('', Validators.required);
   weight = new FormControl('', Validators.required);
-  myGroup= new FormGroup({
-  firstName: new FormControl()
-});
 
-  constructor(private adService: AdService,
+  constructor(private catService: CatService,
               private formBuilder: FormBuilder,
               private http: Http,
               public toast: ToastComponent) { }
 
   ngOnInit() {
-    this.myOptions = [
-      { id: 1, name: 'Screen 1' },
-      { id: 2, name: 'Screen 2' },
-    ];
-
-    this.getAds();
-    this.addAdForm = this.formBuilder.group({
+    this.getCats();
+    this.addCatForm = this.formBuilder.group({
       name: this.name,
       age: this.age,
-      weight: this.weight,
+      weight: this.weight
     });
   }
 
-  onChange() {
-    console.log(this.optionsModel);
-    this.screenIds = this.optionsModel;
-  }
-
-  getAds() {
-    this.adService.getAds().subscribe(
-      data => this.ads = data,
+  getCats() {
+    this.catService.getCats().subscribe(
+      data => this.cats = data,
       error => console.log(error),
       () => this.isLoading = false
     );
   }
 
-  addAd() {
-    this.adService.addAd(this.addAdForm.value).subscribe(
+  addCat() {
+    this.catService.addCat(this.addCatForm.value).subscribe(
       res => {
-        const newAd = res.json();
-        this.ads.push(newAd);
-        this.addAdForm.reset();
+        const newCat = res.json();
+        this.cats.push(newCat);
+        this.addCatForm.reset();
         this.toast.setMessage('item added successfully.', 'success');
       },
       error => console.log(error)
     );
   }
 
-  enableEditing(ad) {
+  enableEditing(cat) {
     this.isEditing = true;
-    this.ad = ad;
+    this.cat = cat;
   }
 
   cancelEditing() {
     this.isEditing = false;
-    this.ad = {};
+    this.cat = {};
     this.toast.setMessage('item editing cancelled.', 'warning');
     // reload the ads to reset the editing
-    this.getAds();
+    this.getCats();
   }
 
-  editAd(ad) {
-    this.adService.editAd(ad).subscribe(
+  editCat(cat) {
+    this.catService.editCat(cat).subscribe(
       res => {
         this.isEditing = false;
-        this.ad = ad;
+        this.cat = cat;
         this.toast.setMessage('item edited successfully.', 'success');
       },
       error => console.log(error)
     );
   }
 
-  deleteAd(ad) {
+  deleteCat(cat) {
     if (window.confirm('Are you sure you want to permanently delete this item?')) {
-      this.adService.deleteAd(ad).subscribe(
+      this.catService.deleteCat(cat).subscribe(
         res => {
-          const pos = this.ads.map(elem => elem._id).indexOf(ad._id);
-          this.ads.splice(pos, 1);
+          const pos = this.cats.map(elem => elem._id).indexOf(cat._id);
+          this.cats.splice(pos, 1);
           this.toast.setMessage('item deleted successfully.', 'success');
         },
         error => console.log(error)
--- server/models/ad.ts
+++ server/models/ad.ts
@@ -1,26 +1,11 @@
 import * as mongoose from 'mongoose';
 
-const adSchema = new mongoose.Schema({
+const catSchema = new mongoose.Schema({
   name: String,
   weight: Number,
-  age: Number,
-  screenIds: [Number],
-  images: [String],
-  template: String,
-  duration: Number,
-  timing: {
-    startDate: Date,
-    endDate: Date,
-    daySchedule: [
-      {
-        from: { dayTimeInSeconds: Number},
-        to: { dayTimeInSeconds: Number},
-        days: [Number]
-      }
-    ]
-  },
+  age: Number
 });
 
-const Ad = mongoose.model('Ad', adSchema);
+const Cat = mongoose.model('Cat', catSchema);
 
-export default Ad;
+export default Cat;
